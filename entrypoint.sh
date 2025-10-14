b7c6f942cebb174874ff4d10f55cf0f34408a8b4
Henry Quinde <65327896+hrypasato@users.noreply.github.com>
2025-10-13 22:06:48 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Reemplazar variables de entorno
-envsubst < /etc/zitadel/config.yaml.template > /etc/zitadel/config.yaml
+envsubst < /etc/zitadel/config.yaml > /etc/zitadel/config.yaml
 
 # Iniciar Zitadel
 exec /zitadel start --config /etc/zitadel/config.yaml --masterkeyFromEnv
